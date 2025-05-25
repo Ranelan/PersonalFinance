bfc8702869825c59a34bab0ecc9f2b33cfdf66e4
smangasmashntuli <230070728@mycput.ac.za>
2025-05-25 23:50:00 +0200

    Merged master into my branch

--- src/main/java/za/ac/cput/repository/RecurringTransactionRepository.java
+++ src/main/java/za/ac/cput/repository/RecurringTransactionRepository.java
@@ -1,6 +1,7 @@
 package za.ac.cput.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.RecurringTransaction;
 
 import java.time.LocalDate;
--- src/main/java/za/ac/cput/repository/TransactionRepository.java
+++ src/main/java/za/ac/cput/repository/TransactionRepository.java
@@ -1,6 +1,7 @@
 package za.ac.cput.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Transaction;
 
 import java.time.LocalDate;
