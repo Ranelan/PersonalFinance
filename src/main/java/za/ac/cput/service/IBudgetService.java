73cfde54b5b150ee83328abca37105493bd956cd
Ranelan <raneyclassic@gmail.com>
2025-07-23 23:42:33 +0200

    Budget Service added

---
+++ src/main/java/za/ac/cput/service/IBudgetService.java
@@ -0,0 +1,12 @@
+package za.ac.cput.service;
+
+import za.ac.cput.domain.Budget;
+
+import java.util.List;
+
+public interface IBudgetService extends IService<Budget, Long>{
+
+    List<Budget> findByMonth(String month);
+    List<Budget> findByLimitAmountGreaterThan(double amount);
+    List<Budget> findByYear(String year);
+}
---
+++ src/test/java/za/ac/cput/service/GoalServiceTest.java
@@ -0,0 +1,45 @@
+package za.ac.cput.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GoalServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findByGoalId() {
+    }
+
+    @Test
+    void findByGoalName() {
+    }
+
+    @Test
+    void findByDeadLine() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
