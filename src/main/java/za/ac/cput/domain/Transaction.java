bcd1fccbae0ab81ea38cd7da416ca7a3d407c13b
Lebohangnyanyatsi <lebuhangnyanyantsi@gmail.com>
2025-07-29 15:50:58 +0200

    Update Transaction.java

--- src/main/java/za/ac/cput/domain/Transaction.java
+++ src/main/java/za/ac/cput/domain/Transaction.java
@@ -1,37 +1,47 @@
 /* Transaction.java
-   Transaction POJO class
+   Transaction POJO class with relationships
    Author: Lebuhang Nyanyantsi(222184353)
-   Date: 11 May 2025 */
+   Date: 11 May 2025
+*/
 
 package za.ac.cput.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
+import jakarta.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 public class Transaction {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long transactionId;
+
     private double amount;
     private LocalDate date;
     private String description;
     private String type;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private RegularUser regularUser;
+
+    @OneToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
     public Transaction() {
     }
 
     public Transaction(Long transactionId, double amount, LocalDate date,
-                       String description, String type) {
+                       String description, String type,
+                       RegularUser regularUser, Category category) {
         this.transactionId = transactionId;
         this.amount = amount;
         this.date = date;
         this.description = description;
         this.type = type;
+        this.regularUser = regularUser;
+        this.category = category;
     }
 
     public Transaction(TransactionBuilder builder) {
@@ -40,6 +50,8 @@ public class Transaction {
         this.date = builder.date;
         this.description = builder.description;
         this.type = builder.type;
+        this.regularUser = builder.regularUser;
+        this.category = builder.category;
     }
 
     public Long getTransactionId() {
@@ -62,12 +74,22 @@ public class Transaction {
         return type;
     }
 
+    public RegularUser getRegularUser() {
+        return regularUser;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
     public static class TransactionBuilder {
         private Long transactionId;
         private double amount;
         private LocalDate date;
         private String description;
         private String type;
+        private RegularUser regularUser;
+        private Category category;
 
         public TransactionBuilder setTransactionId(Long transactionId) {
             this.transactionId = transactionId;
@@ -94,12 +116,24 @@ public class Transaction {
             return this;
         }
 
+        public TransactionBuilder setRegularUser(RegularUser regularUser) {
+            this.regularUser = regularUser;
+            return this;
+        }
+
+        public TransactionBuilder setCategory(Category category) {
+            this.category = category;
+            return this;
+        }
+
         public TransactionBuilder copy(Transaction transaction) {
             this.transactionId = transaction.transactionId;
             this.amount = transaction.amount;
             this.date = transaction.date;
             this.description = transaction.description;
             this.type = transaction.type;
+            this.regularUser = transaction.regularUser;
+            this.category = transaction.category;
             return this;
         }
 
