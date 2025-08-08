8236b01b0a6cce3ab17feaa980ec99179f19dff1
Ranelan <raneyclassic@gmail.com>
2025-08-08 14:35:00 +0200

    permissions updated

--- src/main/java/za/ac/cput/service/IAdminService.java
+++ src/main/java/za/ac/cput/service/IAdminService.java
@@ -1,6 +1,8 @@
 package za.ac.cput.service;
 
 import za.ac.cput.domain.Admin;
+import za.ac.cput.domain.RegularUser;
+
 import java.util.List;
 
 
@@ -9,4 +11,10 @@ public interface IAdminService extends IService<Admin, Long> {
     List<Admin> findByUserName(String userName);
     List<Admin> findByEmail(String email);
     List<Admin> findAll();
+
+    //Regular USer
+    List<RegularUser> viewAllRegularUsers();
+    void deleteRegularUserById(Long id);
+    void updateRegularUserById(Long id, RegularUser regularUser);
+
 }
--- src/main/java/za/ac/cput/service/ITransactionService.java
+++ src/main/java/za/ac/cput/service/ITransactionService.java
@@ -5,12 +5,15 @@
 package za.ac.cput.service;
 
 import za.ac.cput.domain.Transaction;
+
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ITransactionService extends IService<Transaction, Long> {
-    Transaction create(Transaction transaction);
-    Transaction read(Long id);
-    Transaction update(Transaction transaction);
-    boolean delete(Long id);
+
     List<Transaction> findAll();
+    List<Transaction> findByType(String type);
+    List<Transaction> findByDateBetween(LocalDate startDate, LocalDate endDate);
+
+
 }
