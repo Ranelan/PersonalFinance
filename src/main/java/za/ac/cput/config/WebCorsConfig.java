1d73958032aa15bd9574766d70f9c6c778d46208
SceloKevinNyandeni <scelokevinnyandeni22@gmail.com>
2025-10-19 16:41:38 +0200

    Added the Config files

---
+++ src/main/java/za/ac/cput/config/WebCorsConfig.java
@@ -0,0 +1,29 @@
+package za.ac.cput.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+@Configuration
+public class WebCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/")
+                .allowedOrigins("http://localhost:3000", "http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("content-type"
+                        ,"authorization"
+                        ,"Accept"
+                        , "Origin"
+                        , "X-Requested-With"
+                        ,"Access-Control-Request-Method"
+                        ,"Access-Control-Request-Headers"
+                )
+                .exposedHeaders("Access-Control-Allow-Origin")
+                .allowCredentials(true)
+                .maxAge(3600);
+}
+}
